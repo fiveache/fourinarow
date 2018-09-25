e1370b6bfc1cf7c9c665308623c346678f752206
David Lacho <david.lacho@gmail.com>
2018-09-25 12:39:29 -0700

    createTokens function in Player class

--- js/Player.js
+++ js/Player.js
@@ -1,14 +1,28 @@
+import Token from './Token';
 export default class Player {
   constructor(score, id, color, isTurn = false) {
     this.score = score;
     this.id = id;
     this.color = color;
     this.isTurn = isTurn;
-    this.tokens = createTokens(21);
-    this.createTokens = (num) => Array.apply(null, Array(num)).map((e, i) => i);
+    this.tokens = this.createTokens(21);
     this.play = () => this.isTurn
       ? !this.isTurn
       : this.isTurn;
   }
 
+  /**
+  * Creates token objects for player
+  * @param   {integer}   num - Number of token objects to be created
+  * @return  {array}     tokens - an array of new token objects
+  */
+  
+  createTokens(num) {
+    const tokens = [];
+    for (let i = 0; i < num; i++) {
+      let token = new Token(i, this);
+      tokens.push(token);
+    }
+    return tokens;
+  }
 }
